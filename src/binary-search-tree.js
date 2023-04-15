d52bb40e7684efd28a582e7572659899c946946c
volsvik <rudyard47@gmail.com>
2023-04-16 00:23:26 +0300

    feat: task work

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+// const { NotImplementedError } = require('../extensions/index.js');
 
 // const { Node } = require('../extensions/list-tree.js');
 
@@ -6,42 +6,86 @@ const { NotImplementedError } = require('../extensions/index.js');
 * Implement simple binary search tree according to task description
 * using Node from extensions
 */
+class Node {
+  constructor(data) {
+    this.data = data;
+  }
+}
+
 class BinarySearchTree {
+  constructor() {
+      this.arr1 = [];
+  }
+
+  add(number) {
+    const newNode = new Node(number);
+    this.arr1.push(newNode)
+  }
 
   root() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if (this.arr1.length == 0) {
+      return null
+    } else {
+      return this.arr1[0]
+    }
   }
 
-  add(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+  find(number) {
+    let rez = 0
+    for (let i = 0; i < this.arr1.length; i++) {
+      if (this.arr1[i].data == number) {
+        rez = this.arr1[i]
+        i = this.arr1.length
+      } else {
+        rez = null
+      }
+      }
+      return rez
+    }
 
-  has(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+    has(number) {
+      let rez = 0
+      for (let i = 0; i < this.arr1.length; i++) {
+        if (this.arr1[i].data == number) {
+          rez = true
+          i = this.arr1.length
+        } else {
+          rez = false
+        }
+        }
+        return rez
+      }
+      
+    remove(number) {
+      let rez = 0
+      for (let i = 0; i < this.arr1.length; i++) {
+        if (this.arr1[i].data == number) {
+          rez = i
+          i = this.arr1.length
+        }
+        }
+        this.arr1.splice(rez, 1);
+      }
 
-  find(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+      max() {
+        let rez = this.arr1[0].data
+        for (let i = 0; i < this.arr1.length; i++) {
+          if (this.arr1[i].data > rez) {
+            rez = this.arr1[i].data
+        }
+        }
+        return rez
+      }
 
-  remove(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
-
-  min() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
-
-  max() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+      min() {
+        let rez = this.arr1[0].data
+        for (let i = 0; i < this.arr1.length; i++) {
+          if (this.arr1[i].data < rez) {
+            rez = this.arr1[i].data
+          }
+          }
+          return rez
+        }
 }
 
 module.exports = {
