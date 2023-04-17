5ecd9470175fb653ff3a80913fc94bd2322cb7a0
volsvik <rudyard47@gmail.com>
2023-04-18 02:20:03 +0300

    feat: task work

--- src/remove-from-list.js
+++ src/remove-from-list.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+// const { NotImplementedError } = require('../extensions/index.js');
 
 // const { ListNode } = require('../extensions/list-node.js');
 
@@ -22,9 +22,13 @@ const { NotImplementedError } = require('../extensions/index.js');
  *   }
  * }
  */
-function removeKFromList(/* l, k */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+ function removeKFromList(l, k) {
+  let node = l;
+  while (node.next) {
+    node.next.value === k ? node.next = node.next.next : node = node.next
+  }
+  l.value === k ?  l = l.next : ''
+  return l;
 }
 
 module.exports = {
